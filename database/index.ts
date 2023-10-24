1ffb24b3f68b472ebccc33040c2d8ebe4547d7e5
Nahuel <nahuel_alfaro1@outlook.com>
2023-10-24 13:19:36 -0300

    fix deploy

--- database/index.ts
+++ database/index.ts
@@ -2,7 +2,7 @@ import * as express from "express"
 import * as path from "path";
 import * as cors from "cors";
 const app = express()
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "../dist")));
 app.use(cors());
